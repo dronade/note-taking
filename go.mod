54e40d8f5d9a72621d97b517a698708e38ca6486
Emily Canto <56757680+dronade@users.noreply.github.com>
2022-06-29 23:15:57 +0100

    added create note functionality

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module note-taking/Github/Documents
 go 1.16
 
 require (
-	github.com/andlabs/ui v0.0.0-20200610043537-70a69d6ae31e // indirect
+	github.com/andlabs/ui v0.0.0-20200610043537-70a69d6ae31e
 	github.com/mattn/go-sqlite3 v1.14.12
-	github.com/maxence-charriere/go-app/v9 v9.3.3 // indirect
 )
--- main.go
+++ main.go
@@ -18,26 +18,23 @@ import (
 // decide how to go about implementing the frontend
 
 func main() {
-	ui.Main(setupUI)
-	os.Remove("sqlite-database.db")
-
-	log.Println("Creating sqlite-database.db...")
-	file, err := os.Create("sqlite-database.db")
-	if err != nil {
-		log.Fatal(err.Error())
+	// check if table exists
+	// if not, create table
+	if _, err := os.Stat("sqlite-database.db"); os.IsNotExist(err) {
+		file, err2 := os.Create("sqlite-database.db")
+		if err2 != nil {
+			log.Fatal(err2.Error())
+		}
+		file.Close()
 	}
-	file.Close()
-	log.Println("sqlite-database.db created")
-
-	sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db")
-	defer sqliteDatabase.Close()
-	createTable(sqliteDatabase)
-	insertNote(sqliteDatabase, "very important stuff!!!", "this, this, this and this")
-	insertNote(sqliteDatabase, "blah", "blh blah blah blah blah")
-	insertNote(sqliteDatabase, "ahem", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan lobortis arcu, rhoncus cursus augue malesuada in. Sed eget bibendum magna")
-	insertNote(sqliteDatabase, "to be deleted", "if this is stil in the table something's gone wrong :(")
-	deleteNote(sqliteDatabase)
-	displayNotes(sqliteDatabase)
+	createTable()
+	ui.Main(setupUI)
+	// insertNote("very important stuff!!!", "this, this, this and this")
+	// insertNote("blah", "blh blah blah blah blah")
+	// insertNote("ahem", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan lobortis arcu, rhoncus cursus augue malesuada in. Sed eget bibendum magna")
+	// insertNote("to be deleted", "if this is stil in the table something's gone wrong :(")
+	// deleteNote(sqliteDatabase)
+	// displayNotes(sqliteDatabase)
 }
 
 var mainwin *ui.Window
@@ -62,9 +59,21 @@ func makeCreateNotePage() ui.Control {
 	entryForm.SetPadded(true)
 	group.SetChild(entryForm)
 
-	entryForm.Append("Title", ui.NewEntry(), false)
-	entryForm.Append("Enter Note:", ui.NewMultilineEntry(), true)
-	vbox.Append(ui.NewButton("Create", ), false)
+	noteTitle := ui.NewEntry()
+	noteContent := ui.NewMultilineEntry()
+	entryForm.Append("Title", noteTitle, false)
+	entryForm.Append("Enter Note:", noteContent, true)
+
+	button := ui.NewButton("Create")
+
+	vbox.Append(button, false)
+	button.OnClicked(func(*ui.Button) {
+		log.Println(noteTitle.Text())
+		log.Println(noteContent.Text())
+		go insertNote(noteTitle.Text(), noteContent.Text())
+		noteTitle.SetText("")
+		noteContent.SetText("")
+	})
 
 	return vbox
 }
@@ -108,24 +117,32 @@ func setupUI() {
 	mainwin.Show()
 }
 
-func createTable(db *sql.DB) {
-	createNoteTableSQL := `CREATE TABLE notes (
+func createTable() {
+	db, _ := sql.Open("sqlite3", "./sqlite-database.db")
+	defer db.Close()
+	createNoteTableSQL := `CREATE TABLE IF NOT EXISTS notes (
 		"idNote" integer NOT NULL PRIMARY KEY AUTOINCREMENT,		
 		"title" TEXT,
 		"content" TEXT		
 	  );`
-
-	log.Println("create notes table")
 	statement, err := db.Prepare(createNoteTableSQL)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	statement.Exec()
-	log.Println("notes table created")
 }
 
-func insertNote(db *sql.DB, title string, content string) {
+func insertNote(title string, content string) {
 	log.Println("inserting notes")
+
+	// check if string is empty
+	if title == "" || content == "" {
+		log.Println("title or content is empty")
+		return
+	}
+
+	db, _ := sql.Open("sqlite3", "./sqlite-database.db")
+	defer db.Close()
 	insertNoteSQL := `INSERT INTO notes(title, content) VALUES (?, ?)`
 	statement, err := db.Prepare(insertNoteSQL)
 	if err != nil {
@@ -137,7 +154,9 @@ func insertNote(db *sql.DB, title string, content string) {
 	}
 }
 
-func displayNotes(db *sql.DB) {
+func displayNotes() {
+	db, _ := sql.Open("sqlite3", "./sqlite-database.db")
+	defer db.Close()
 	row, err := db.Query("SELECT * FROM notes ORDER BY title")
 	if err != nil {
 		log.Fatal(err)
@@ -152,8 +171,9 @@ func displayNotes(db *sql.DB) {
 	}
 }
 
-func deleteNote(db *sql.DB) {
-	log.Println("deleting note")
+func deleteNote() {
+	db, _ := sql.Open("sqlite3", "./sqlite-database.db")
+	defer db.Close()
 	deleteNoteSQL := `DELETE FROM notes WHERE title="to be deleted"`
 	statement, err := db.Prepare(deleteNoteSQL)
 	if err != nil {
